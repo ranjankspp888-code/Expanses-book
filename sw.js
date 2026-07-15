e690d08afa9533fb78148b21c8d9328c33b186f9
ranjankspp888-code <ranjankspp888@gmail.com>
2026-07-15 20:36:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,43 @@
+const CACHE_NAME = 'exp-store-v1';
+const ASSETS_TO_CACHE = [
+  './index.html', 
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('exp-store').then((cache) => cache.addAll(['./index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
+
+// Purana cache delete karne ke liye 'activate' event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  // Google Apps Script ko cache nahi karna hai, seedha network se fetch karega
+  if (e.request.url.includes('script.google.com')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
+  // Baki files ke liye pehle cache check karega, agar nahi mili to internet (fetch) use karega
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
